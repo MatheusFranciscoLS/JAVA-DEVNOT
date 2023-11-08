5a1a6d26130852fa92dc504c6a4c9e75ae18611e
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-11-08 19:20:41 -0300

    08/11/23

--- Java2-Semestre/JDBCOutrosTeste/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        ConnectionDAO obj = new ConnectionDAO();
-        obj.cleanup();
-        obj.doexample();
-    }
-}
