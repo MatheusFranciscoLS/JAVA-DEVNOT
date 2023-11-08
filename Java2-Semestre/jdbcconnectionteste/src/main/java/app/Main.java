4bdc27922797364bdae0d83009aee4ece12fe256
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-11-08 19:17:40 -0300

    08/11/23

--- Java2-Semestre/jdbcconnectionteste/src/main/java/app/Main.java
+++ Java2-Semestre/jdbcconnectionteste/src/main/java/app/Main.java
@@ -5,6 +5,6 @@ import Connection.ConnectionDAO;
 public class Main {
     public static void main(String[] args) {
         new ConnectionDAO().criarTabela();
-        new ConnectionDAO().inserir("Daniel", "romanodaniel1012@gmail.com");
+        new ConnectionDAO().inserir("Matheus", "matheus@gmail.com");
     }
 }
